80e76337d3dfefa2f3ead450060c3b0fffd51e7f
VASUDEV KANUNGO <77634422+vasu2705@users.noreply.github.com>
2021-08-25 13:40:16 +0530

    services update

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
-import service1 from "../img/service1.svg";
+import service1 from "../img/service3.svg";
 import service2 from "../img/service2.svg";
 
 const aboutSection = {
@@ -21,7 +21,9 @@ const upperSection = {
   color: "#fff",
   textAlign: "center"
 };
-
+const servicesimg ={
+  height:"200px"
+}
 const upperCards = {
   margin: "1rem",
   border: "2px solid white",
@@ -42,7 +44,7 @@ const Services = () => {
       
         <Row>
         <Col md={2} lg={3}>
-            < img style={{height:"200px"}} src = {service1} alt="serivces1" />
+            <img style={servicesimg} src = {service1} alt="serivces1" />
         </Col>
         <Col xs={12} md={8} lg={6}>
       <h5 style={aboutSection}>Our Services</h5>
