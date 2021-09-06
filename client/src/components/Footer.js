ce93858c3270ec63a34db69aba85910adceff737
VASUDEV KANUNGO <77634422+vasu2705@users.noreply.github.com>
2021-09-06 21:53:20 +0530

    changes

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,38 +4,40 @@ import logo from "../img/logo3.png";
 const Footer = () => {
   return (
     <>
-      <footer>
-        <div className="footer">
-          <ul className="ul-list">
-            <li>
-              <NavLink to="/TnC">Terms {'&'} Conditions</NavLink>
-            </li>
-            <li>
-              <NavLink to="/FAQ">FAQ</NavLink>
-            </li>
-            <li>
-              <NavLink to="/Policies">Policies</NavLink>
-            </li>
-            <li>
-              <NavLink to="/RTI">RTI</NavLink>
-            </li>
-            <li>
-              <NavLink to="#">Join Us</NavLink>
-            </li>
-            <li>
-              <NavLink to="/Feedback">Feedback</NavLink>
-            </li>
-          </ul>
+      <footer className="footer">
+        <div className="footer1">
+          <div className="ul">
+            <ul>
+              <li>
+                <NavLink to="/TnC">Terms & Conditions</NavLink>
+              </li>
+              <li>
+                <NavLink to="/FAQ">FAQ</NavLink>
+              </li>
+              <li>
+                <NavLink to="/Policies">Policies</NavLink>
+              </li>
+              <li>
+                <NavLink to="#">Help</NavLink>
+              </li>
+              <li>
+                <NavLink to="#">Join Us</NavLink>
+              </li>
+              <li className="libg">
+                <NavLink to="/feedback">Feedback</NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="container__footer">
-          <div className="content__footer">
+        <div className="container">
+          <div className="content">
             <img src={logo} alt="logo" />
           </div>
-          <span className="vertical-line__footer"></span>
-          <div className="footer__div">
-            <p className="footer__para">
-              Website Content Managed by Web team of Crime Free Bharat ! <br />
-              Designed, Developed and Hosted by Crime Free Bharat
+          <span className="vertical-line"></span>
+          <div>
+            <p>
+              Web Content Developed and Managed by National Technical Research &
+              Development Committee, Crime Free Bharat
             </p>
           </div>
         </div>
