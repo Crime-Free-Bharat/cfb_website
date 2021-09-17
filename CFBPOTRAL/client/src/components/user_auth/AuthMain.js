69cbc5c0e4051bece49dddc4d8c666c6380214e4
VASUDEV KANUNGO <77634422+vasu2705@users.noreply.github.com>
2021-09-17 16:18:01 +0530

    proxy

--- CFBPOTRAL/client/src/components/user_auth/AuthMain.js
+++ CFBPOTRAL/client/src/components/user_auth/AuthMain.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import {Route, Switch} from 'react-router-dom';
-import Login from './Login';
-import Signup from './Signup';
-import email from './Email_Res';
-import emailver from './Email_Veri';
-import Otp from './Otp';
-import Otpconf from './Otpconf';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Login from "./Login";
+import Signup from "./Signup";
+import email from "./Email_Res";
+import emailver from "./Email_Veri";
+import Otp from "./Otp";
+import Otpconf from "./Otpconf";
 
 const AuthMain = () => (
-    <main>
-        <Switch>
-            <Route exact path='/signup' component={Signup}/>
-            <Route exact path='/login' component={Login}/>
-            <Route exact path='/email' component={email}/>
-            <Route exact path='/emailver' component={emailver}/>
-            <Route exact path='/otp' component={Otp}/>
-            <Route exact path='/otpconf' component={Otpconf}/>
-        </Switch>
-    </main>
+  <main>
+    <Switch>
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/email" component={email} />
+      <Route exact path="/emailver" component={emailver} />
+      <Route exact path="/otp" component={Otp} />
+      <Route exact path="/otpconf" component={Otpconf} />
+    </Switch>
+  </main>
 );
 
 export default AuthMain;
