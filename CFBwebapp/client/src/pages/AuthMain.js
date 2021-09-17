b51aee5106fbfefd0531c775883d9676dc861d1b
VASUDEV KANUNGO <77634422+vasu2705@users.noreply.github.com>
2021-09-17 20:45:01 +0530

    changes

--- CFBwebapp/client/src/pages/AuthMain.js
+++ CFBwebapp/client/src/pages/AuthMain.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Login from "../user_auth/Login";
-import Signup from "../user_auth/Signup";
-import email from "../user_auth/Email_Res";
-import emailver from "../user_auth/Email_Veri";
-import Otp from "../user_auth/Otp";
-import Otpconf from "../user_auth/Otpconf";
+import Login from "../components/user_auth/Login";
+import Signup from "../components/user_auth/Signup";
+import email from "../components/user_auth/Email_Res";
+import emailver from "../components/user_auth/Email_Veri";
+import Otp from "../components/user_auth/Otp";
+import Otpconf from "../components/user_auth/Otpconf";
 
 const AuthMain = () => (
   <>
