e59b3099b2e4787c5669936985ae1e6ca4a15e9b
Jayesh Kalamkar <jvkalamkar@gmail.com>
2021-09-18 07:07:13 +0530

    devdependancies

--- CFBwebapp/controllers/contactController.js
+++ CFBwebapp/controllers/contactController.js
@@ -2,28 +2,28 @@ const conn = require("../db/conn");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userschema");
 
-exports.contact = async (req, res) => {
-  try {
-    const { name, phone, email, subject, message } = req.body;
-    if (!name || !phone || !email || !subject || !message) {
-      return res.json({ error: "plzz filled bhar do yrrr" });
-    } else {
-      const userContact = await User.findOne({ _id: req.userID });
-      if (userContact) {
-        const userMessage = await userContact.addMessage(
-          name,
-          phone,
-          email,
-          subject,
-          message
-        );
+exports.contact = async(req, res) => {
+    try {
+        const { name, phone, email, subject, message } = req.body;
+        if (!name || !phone || !email || !subject || !message) {
+            return res.json({ error: "plzz filled bhar do yrrr" });
+        } else {
+            const userContact = await User.findOne({ _id: req.userID });
+            if (userContact) {
+                const userMessage = await userContact.addMessage(
+                    name,
+                    phone,
+                    email,
+                    subject,
+                    message
+                );
 
-        await userContact.save();
+                await userContact.save();
 
-        res.status(201).json({ message: "successfully saved." });
-      }
+                res.status(201).json({ message: "successfully saved." });
+            }
+        }
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
 };
