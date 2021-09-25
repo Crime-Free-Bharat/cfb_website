fa1d638a77c9a71d1e5c8d85ea1d70214dbc254d
Abhay Singh Chauhan <75360601+arcaneAB@users.noreply.github.com>
2021-09-25 20:15:35 +0530

    Update tnc.js

--- CFBwebapp/client/src/pages/Footer_render/tnc.js
+++ CFBwebapp/client/src/pages/Footer_render/tnc.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
-import TnC from "../../components/footer_pg/FAQ";
+import TnC from "../../components/footer_pg/TnC";
 const tnc = () => {
   return (
     <>
