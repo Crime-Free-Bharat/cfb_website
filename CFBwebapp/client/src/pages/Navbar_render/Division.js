d92028aa65e5f273537f259c76a64e024794a436
Abhay Singh Chauhan <75360601+arcaneAB@users.noreply.github.com>
2021-10-26 14:24:34 +0530

    division page linked

---
+++ CFBwebapp/client/src/pages/Navbar_render/Division.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+import Divisions from "../../components/nav_pages/Division";
+const Division = () => {
+  return (
+    <>
+      <Navbar />
+      <Divisions />
+      <Footer />
+    </>
+  );
+};
+
+export default Division;
