32dba1b331f1edea07cb9ed665ae2520af45b7f4
Abhay Singh Chauhan <75360601+arcaneAB@users.noreply.github.com>
2021-10-27 14:14:21 +0530

    services internal pages creation

---
+++ CFBwebapp/client/src/components/nav_pages/servicesPages/ReportCrimeOnline.js
@@ -0,0 +1,104 @@
+import React from 'react'
+
+export default function ReportCrimeOnline() {
+    return (
+      <div>
+        <section id="" className="section sectionterms">
+          <div className="container containerterms">
+            <br />
+            <div className="row">
+              <div className="col-sm-12">
+                <h3 className="sectionheadact">Report Crime Online</h3>
+                <p className="sectionparaact">
+                  You can report crimes online from Crime free bharat
+                </p>
+              </div>
+            </div>
+            <br />
+            <div className="row">
+              <div className="col-sm-4">
+                <img
+                  // src= {terms}
+                  alt="termsIMG"
+                  className="img-responsive stctionimg"
+                ></img>
+              </div>
+              <div className="col-sm-8">
+                <p style={{ textAlign: "justify" }}>
+                  <b>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Distinctio rerum ex alias consectetur consequatur nam ad
+                    neque. Eaque sint recusandae alias consectetur mollitia?
+                    Sapiente maxime repudiandae explicabo, fuga corporis
+                    repellendus! Lorem ipsum, dolor sit amet consectetur
+                    adipisicing elit. Distinctio rerum ex alias consectetur
+                    consequatur nam ad neque. Eaque sint recusandae alias
+                    consectetur mollitia? Sapiente maxime repudiandae explicabo,
+                    fuga corporis repellendus! Lorem ipsum, dolor sit amet
+                    consectetur adipisicing elit. Distinctio rerum ex alias
+                    consectetur consequatur nam ad neque. Eaque sint recusandae
+                    alias consectetur mollitia? Sapiente maxime repudiandae
+                    explicabo, fuga corporis repellendus!
+                  </b>
+                </p>
+                <ul>
+                  <li>
+                    lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+                  <li>
+                    lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+                  <li>
+                    lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <br />
+            <br />
+            <div className="row">
+              <div className="col-sm-8">
+                <p style={{ textAlign: "justify" }}>
+                  <b>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Distinctio rerum ex alias consectetur consequatur nam ad
+                    neque. Eaque sint recusandae alias consectetur mollitia?
+                    Sapiente maxime repudiandae explicabo, fuga corporis
+                    repellendus! Lorem ipsum, dolor sit amet consectetur
+                    adipisicing elit. Distinctio rerum ex alias consectetur
+                    consequatur nam ad neque. Eaque sint recusandae alias
+                    consectetur mollitia? Sapiente maxime repudiandae explicabo,
+                    fuga corporis repellendus! Lorem ipsum, dolor sit amet
+                    consectetur adipisicing elit. Distinctio rerum ex alias
+                    consectetur consequatur nam ad neque. Eaque sint recusandae
+                    alias consectetur mollitia? Sapiente maxime repudiandae
+                    explicabo, fuga corporis repellendus!
+                  </b>
+                </p>
+                <ul>
+                  <li>
+                    lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+                  <li>
+                    lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+                  <li>
+                    lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+                </ul>
+              </div>
+              <div className="col-sm-4">
+                <img
+                  // src={terms2}
+                  alt="termsIMG"
+                  className="img-responsive stctionimg"
+                ></img>
+              </div>
+            </div>
+            <br />
+            <br />
+          </div>
+        </section>
+      </div>
+    );
+}
